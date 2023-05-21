e529761e3acdbc46aaa69994eef082c24109849b
manojkumar16122 <115546459+manojkumar16122@users.noreply.github.com>
2023-05-21 21:58:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,8 +26,6 @@ st.header("Flowers Recognition")
 
 def homepage():
         with st.expander("Examples"):
-
-  
         col1, col2, col3 = st.columns(3)
         with col1:
             st.image("datasamples/daisy.jpg", width=200, caption="daisy")
